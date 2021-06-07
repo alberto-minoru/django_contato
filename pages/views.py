53872ea28665ca4f543559e1ab6c4485327128c8
Alberto Minoru <alberto.minoru@gmail.com>
2021-06-07 16:58:39 -0300

    Adicionado reCAPTCHA

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,6 @@
+import json
+import urllib
+from decouple import config
 from django.contrib import messages
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, FormView
@@ -14,6 +17,24 @@ class ContatoPageView(FormView):
     success_url = reverse_lazy('contato')
 
     def form_valid(self, form):
-        form.send_mail()
-        messages.success(self.request, 'E-mail enviado com sucesso!')
-        return super(ContatoPageView, self).form_valid(form)
+
+        # Início da validação do reCAPTCHA
+        recaptcha_response = self.request.POST.get('g-recaptcha-response')
+        url = 'https://www.google.com/recaptcha/api/siteverify'
+        values = {
+            'secret': config('GOOGLE_RECAPTCHA_SECRET_KEY'),
+            'response': recaptcha_response,
+        }
+        data = urllib.parse.urlencode(values).encode()
+        req = urllib.request.Request(url, data=data)
+        response = urllib.request.urlopen(req)
+        result = json.loads(response.read().decode())
+        # Fim da validação do reCAPTCHA
+
+        if result['success']:
+            form.send_mail()
+            messages.success(self.request, 'E-mail enviado com sucesso!')
+            return super(ContatoPageView, self).form_valid(form)
+        else:
+            messages.error(self.request, 'Captcha inválido. Por favor tente novamente.')
+            return super(ContatoPageView, self).form_invalid(form)
